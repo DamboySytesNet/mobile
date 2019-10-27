2ee0912e29a1a9c1fed2f551ea4595016e96be40
216791 Karbowski Artur <216791@edu.p.lodz.pl>
2019-10-27 16:02:31 +0100

    Add animation to bell and add number od notifications

--- app/activities/employee/cockpit/cockpit.js
+++ app/activities/employee/cockpit/cockpit.js
@@ -1,31 +1,56 @@
 const observableModule = require("tns-core-modules/data/observable");
+const frameModule = require('tns-core-modules/ui/frame');
 
-let testConsultation = [
-    {
-        id: 1,
-        subject: 'Podstawy programowania',
-        teacher: 'Gabriela Narutowicz',
-        room: 243,
-        date: new Date(2019, 10, 30, 10, 30),
-    },
-    {
-        id: 2,
-        subject: 'Podstawy sieci komputerowych',
-        teacher: 'Bł Dziuba',
-        room: 270,
-        date: new Date(2019, 10, 25, 10, 30)
-    }
-];
+// let testConsultation = [
+//     {
+//         id: 1,
+//         student: 'Adrian Adriański',
+//         room: 243,
+//         date: new Date(2019, 10, 30, 10, 30),
+//     },
+//     {
+//         id: 2,
+//         student: 'Damian Kołek',
+//         room: 211,
+//         date: new Date(2019, 11, 22, 8, 15),
+//     }
+// ];
 
-let employee = {
-    id: 123456,
-    name: 'testName',
-    surname: 'testSurname',
-    consultations: testConsultation,
-}
+// let employee = {
+//     id: 123456,
+//     name: 'testName',
+//     surname: 'testSurname',
+//     consultations: testConsultation,
+// }
 
 let pageData = new observableModule.fromObject({
-    user: ''
+    user: '',
+    notifications: 0,
+
+
+    // w celach testowych:
+    alertNotification() {
+       this.notifications++;
+       animateBell();
+    },
+    goToEmployeeConsultations(){
+        let moduleName = 'activities/employee/consultations/consultations';
+        const navigationEntry = {
+            moduleName: moduleName,
+            context: {
+                user: user,
+            }
+        };
+
+        frameModule.topmost().navigate(navigationEntry);
+    },
+    goToEmployeeSubjects(){
+        //frameModule.topmost().navigate(navigationEntry);
+    },
+    goToEmployeeSettings(){
+        //frameModule.topmost().navigate(navigationEntry);
+    }
+
 });
 
 exports.exit = (args) => {
@@ -38,7 +63,31 @@ exports.exit = (args) => {
 exports.pageLoaded = (args) => {
     let page = args.object;
     const context = page.navigationContext;
-
     pageData.set('user', `${context.name} ${context.surname}`);
-    page.bindingContext = pageData;
+    page.bindingContext = pageData; 
+    bells = page.getViewById("bell");
+}
+
+
+
+
+function animateBell(){
+    bells.animate({
+        rotate: 40,
+        duration: 270,
+        scale: {x: 1.3, y: 1.3},
+    })
+    .then(() => { 
+        return bells.animate({
+            rotate: -40,
+            duration: 270,
+        });
+    })
+    .then(() => { 
+        return bells.animate({
+            rotate: 0,
+            duration: 270,
+            scale: {x: 1, y: 1},
+        });
+    });
 }
