a334de95b791a8f7cfb5101df289fcf79c0cfebf
Damian K <damian-8-8@o2.pl>
2019-11-07 20:24:28 +0100

    dynamo base

--- app/activities/employee/settings/settings.js
+++ app/activities/employee/settings/settings.js
@@ -1,24 +1,48 @@
 const observableModule = require("tns-core-modules/data/observable");
 const frameModule = require('tns-core-modules/ui/frame');
+let page;
 
-const myHours = {
-    
-};
+let myHours = [
+    {
+        from: '8:00',
+        to: '8:01',
+        day: 'Poniedziałek',
+        room: '202',
+        edit() {
+
+        },
+
+        remove() {
+            
+        }
+    },
+    {
+        from: '8:02',
+        to: '8:03',
+        day: 'Wtorek',
+        room: '204',
+        edit() {
+
+        },
+
+        remove() {
+            
+        }
+    }
+];
 
 let pageData = new observableModule.fromObject({
     user: '',
-    room: '213'
+    room: '213',
+    myHours: myHours
 });
 
 exports.exit = (args) => {
-    const button = args.object;
-    const page = button.page;
-    
     page.frame.goBack();
 }
 
 exports.pageLoaded = (args) => {
-    let page = args.object;
+    page = args.object;
     const context = page.navigationContext;
     pageData.set('user', `${context.name} ${context.surname}`);
     page.bindingContext = pageData; 
