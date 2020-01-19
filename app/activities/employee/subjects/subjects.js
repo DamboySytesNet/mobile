cced65fd537d98f284d803724b12f635939723cf
Karbowskii <arturkarbowskitest@gmail.com>
2020-01-19 04:49:24 +0100

    Everything in employee is using db now (Please approve :>)

--- app/activities/employee/subjects/subjects.js
+++ app/activities/employee/subjects/subjects.js
@@ -24,11 +24,31 @@ exports.back = (args) => {
 exports.pageLoaded = (args) => {
     console.log(u.user.token);
     page = args.object;
+    let userSubjects = [];
+    let withoutUserSubjects = [];
+    let tmp = [];
+    let subject;
     if (!u.user.subjects.loaded) {
-        SubjectsEmployeeHttpRequest.get(u.user.id, u.user.token)
-        .then(res =>{
-            u.user.subjects.data.withoutUserSubjects = loadEmployeeSubjects(res);
-            u.user.subjects.data.userSubjects = loadEmployeeSubjects(res);
+        SubjectsEmployeeHttpRequest.get(u.user.token)
+        .then(res => {
+            for (let it of res) {
+                subject = tmp.find(el => {return el.id === it.subjectId});
+                if (subject) {
+                    subject.employees.push({id: it.userId, name: it.userName, surname: it.userSurname});
+                }else {
+                    let employees = [{id: it.userId, name: it.userName, surname: it.userSurname}];
+                    tmp.push({id: it.subjectId, title: it.subjectName, semester: it.semester,employees: employees});
+                }
+            }
+            for (let it of tmp) {
+                subject = it.employees.find(el => {return el.id === u.user.id})
+                if (subject) 
+                    userSubjects.push(it);
+                else
+                    withoutUserSubjects.push(it);
+            }
+            u.user.subjects.data.withoutUserSubjects = loadEmployeeSubjects(withoutUserSubjects);
+            u.user.subjects.data.userSubjects = loadEmployeeSubjects(userSubjects);
             u.user.subjects.loaded = true;   
             pageData.set('withoutUserSubjects', u.user.subjects.data.withoutUserSubjects);
             pageData.set('userSubjects', u.user.subjects.data.userSubjects)
@@ -36,12 +56,11 @@ exports.pageLoaded = (args) => {
             withoutUserListView = page.getViewById('withoutUserListView');
             userListView = page.getViewById('userListView');
         })
-        .catch(() => {
-
+        .catch((error) => {
+            alert('Nie udalo sie wczytać przedmiotów !' + error.message);
+            page.frame.goBack();
         });
     }else {
-        // sortByTitle(testSubjects);
-        // sortByTitle(testUserSubjects);
         pageData.set('withoutUserSubjects', u.user.subjects.data.withoutUserSubjects);
         pageData.set('userSubjects', u.user.subjects.data.userSubjects)
         page.bindingContext = pageData;
@@ -72,43 +91,51 @@ exports.changeIsOpen = (args) => {
 
 exports.addToUserSubjects = (args) => {
     let id = args.object.index;
-    let item = pageData.withoutUserSubjects.find(el => el.id === id);
-
-    let userData = {
-        id: u.user.id,
-        name: u.user.name,
-        surname: u.user.surname
-    };
-    let emp = item.employees.find(el => el.id === u.user.id);
-    let indexEmp = item.employees.indexOf(emp);
-    if (indexEmp === -1){
-        item.employees.push(userData);
-        item.height += 54;
-    }
-
-    moveItemFromListToOtherList(item, pageData.withoutUserSubjects, pageData.userSubjects);
-    sortByTitle(pageData.userSubjects);
-    sortByTitle(pageData.withoutUserSubjects);
-    userListView.refresh();
-    withoutUserListView.refresh();
+    SubjectsEmployeeHttpRequest.add(u.user.id, u.user.token, id)
+    .then( () => {
+        let item = pageData.withoutUserSubjects.find(el => el.id === id);
+        let userData = {
+            id: u.user.id,
+            name: u.user.name,
+            surname: u.user.surname
+        };
+        let emp = item.employees.find(el => el.id === u.user.id);
+        let indexEmp = item.employees.indexOf(emp);
+        if (indexEmp === -1){
+            item.employees.push(userData);
+            item.height += 54;
+        }
+        moveItemFromListToOtherList(item, pageData.withoutUserSubjects, pageData.userSubjects);
+        sortByTitle(pageData.userSubjects);
+        sortByTitle(pageData.withoutUserSubjects);
+        userListView.refresh();
+        withoutUserListView.refresh();
+    })
+    .catch( () => {
+        alert("Nie udało się dodać użytkownika do przedmiotu!")
+    });
 }
 
 exports.removeFromUserSubjects = (args) => {
     let id = parseInt(args.object.index, 10);
-    let item = pageData.userSubjects.find(el => el.id === id);
-
-    let emp = item.employees.find(el => el.id === u.user.id);
-    let indexEmp = item.employees.indexOf(emp);
-    if (indexEmp >= 0){
-        item.employees.splice(indexEmp, 1);
-        item.height -= 54;
-    }
-
-    moveItemFromListToOtherList(item, pageData.userSubjects, pageData.withoutUserSubjects);
-    sortByTitle(pageData.userSubjects);
-    sortByTitle(pageData.withoutUserSubjects);
-    userListView.refresh();
-    withoutUserListView.refresh();
+    SubjectsEmployeeHttpRequest.remove(u.user.id, u.user.token, id)
+    .then( () => {
+        let item = pageData.userSubjects.find(el => el.id === id);
+        let emp = item.employees.find(el => el.id === u.user.id);
+        let indexEmp = item.employees.indexOf(emp);
+        if (indexEmp >= 0){
+            item.employees.splice(indexEmp, 1);
+            item.height -= 54;
+        }
+        moveItemFromListToOtherList(item, pageData.userSubjects, pageData.withoutUserSubjects);
+        sortByTitle(pageData.userSubjects);
+        sortByTitle(pageData.withoutUserSubjects);
+        userListView.refresh();
+        withoutUserListView.refresh();
+    })
+    .catch(() => {
+        alert("Nie udało się usunać użytkownika z przedmiotu!")
+    });
 }
 
 function moveItemFromListToOtherList(item, from, to) {
--- app/modules/request/subjectsEmployeeHttpRequest.js
+++ app/modules/request/subjectsEmployeeHttpRequest.js
@@ -1,28 +1,97 @@
 const httpModule = require('tns-core-modules/http');
 
-exports.get = (id, token) => {
+exports.get = (token) => {
     return new Promise((revoke, reject) => {
-        if(!id || !token) {
+        if(!token) {
             reject('Niepoprawne żądanie');
             return;
         }  
 
         httpModule.request({
-            url: `https://damboy.sytes.net/mk/getSubjects.php`,
+            url: `https://damboy.sytes.net/mk/getSubjectsLecturers.php`,
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             content: JSON.stringify({
-                user_id: id,
                 token: token
             })
         }).then( res => {
             if (res.statusCode === 200) {
                 try {
                     const json = JSON.parse(res.content);
-                    console.log(json);
                     if (json.status === 'success') {
-                        const result = JSON.parse(json.msg);
-                        revoke(result);
+                        revoke(json.msg);
+                    }
+                    else {
+                        console.log('Błąd');
+                        reject(json.msg)
+                    }
+                }
+                catch (e){
+                    console.log(e);
+                }
+            }
+        })
+    })
+}
+
+exports.add = (user_id, token, subject_id) => {
+    return new Promise((revoke, reject) => {
+        if(!token || !user_id || !subject_id) {
+            reject('Niepoprawne żądanie');
+            return;
+        } 
+
+        httpModule.request({
+            url: `https://damboy.sytes.net/mk/addLecturerSubject.php`,
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            content: JSON.stringify({
+                user_id, user_id,
+                token: token,
+                subject_id: subject_id
+            })
+        }).then( res => {
+            if (res.statusCode === 200) {
+                try {
+                    const json = JSON.parse(res.content);
+                    if (json.status === 'success') {
+                        revoke(json.msg);
+                    }
+                    else {
+                        console.log('Błąd');
+                        reject(json.msg)
+                    }
+                }
+                catch (e){
+                    console.log(e);
+                }
+            }
+        })
+    })
+}
+
+exports.remove = (user_id, token, subject_id) => {
+    return new Promise((revoke, reject) => {
+        if(!token || !user_id || !subject_id) {
+            reject('Niepoprawne żądanie');
+            return;
+        }  
+
+        httpModule.request({
+            url: `https://damboy.sytes.net/mk/removeLecturerSubject.php`,
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            content: JSON.stringify({
+                user_id, user_id,
+                token: token,
+                subject_id: subject_id
+            })
+        }).then( res => {
+            if (res.statusCode === 200) {
+                try {
+                    const json = JSON.parse(res.content);
+                    if (json.status === 'success') {
+                        revoke(json.msg);
                     }
                     else {
                         console.log('Błąd');
