1cacd178f75cb5c7af96b4724a252c9e0e61cb7c
damboysytesnet <damian-8-8@o2.pl>
2020-01-19 21:58:45 +0100

    password changing

--- app/activities/employee/cockpit/cockpit.js
+++ app/activities/employee/cockpit/cockpit.js
@@ -1,6 +1,8 @@
 const observableModule = require("tns-core-modules/data/observable");
 const frameModule = require('tns-core-modules/ui/frame');
+const dialogsModule = require('tns-core-modules/ui/dialogs');
 
+const auth = require('~/modules/auth/auth');
 const u = require('~/common/data/user');
 const logout = require('~/modules/utils/logout');
 
@@ -12,12 +14,12 @@ let pageData = new observableModule.fromObject({
 
     // w celach testowych:
     alertNotification() {
-       this.numberOfNotifications++;
-       animateBell();
+        this.numberOfNotifications++;
+        animateBell();
 
-       pageData.consultations[0]
+        pageData.consultations[0]
     },
-    
+
     goToEmployeeConsultations() {
         let moduleName = 'activities/employee/consultations/consultations';
         const navigationEntry = {
@@ -32,9 +34,9 @@ let pageData = new observableModule.fromObject({
     },
 
     goToEmployeeSettings() {
-        frameModule.topmost() .navigate(
-            { moduleName: 'activities/employee/settings/settings' }
-        );
+        frameModule.topmost().navigate({
+            moduleName: 'activities/employee/settings/settings'
+        });
     }
 
 });
@@ -49,30 +51,76 @@ exports.exit = (args) => {
 exports.pageLoaded = (args) => {
     let page = args.object;
     pageData.set('user', `${u.user.name} ${u.user.surname}`);
-    page.bindingContext = pageData; 
+    page.bindingContext = pageData;
     bells = page.getViewById('bell');
 }
 
-
+exports.changePassword = () => {
+    // Prompot user for new password
+    dialogsModule.prompt({
+        title: 'Ustawianie hasła',
+        message: 'Podaj nowe hasło',
+        inputType: 'password',
+        defaultText: '',
+        okButtonText: 'Ok',
+        cancelButtonText: 'Cancel'
+    }).then((data) => {
+        if (data.result) {
+            dialogsModule.prompt({
+                title: 'Ustawianie hasła',
+                message: 'Podaj nowe ponownie hasło',
+                inputType: 'password',
+                defaultText: '',
+                okButtonText: 'Ok',
+                cancelButtonText: 'Cancel'
+            }).then((data2) => {
+                if (data.result) {
+                    if (data.text === data2.text) {
+                        // Send request
+                        auth.changePassword(u.user.id, u.user.token, data.text)
+                            .then((msg) => {
+                                alert(msg);
+                            })
+                            .catch((msg) => {
+                                alert(msg);
+                            });
+                    } else {
+                        alert('Hasła się nie zgadzają!');
+                    }
+                }
+            }).catch((msg) => {
+                alert(msg);
+            });
+        }
+    }).catch((msg) => {
+        alert(msg);
+    });
+}
 
 
 function animateBell() {
     bells.animate({
-        rotate: 40,
-        duration: 270,
-        scale: {x: 1.3, y: 1.3},
-    })
-    .then(() => { 
-        return bells.animate({
-            rotate: -40,
+            rotate: 40,
             duration: 270,
+            scale: {
+                x: 1.3,
+                y: 1.3
+            },
+        })
+        .then(() => {
+            return bells.animate({
+                rotate: -40,
+                duration: 270,
+            });
+        })
+        .then(() => {
+            return bells.animate({
+                rotate: 0,
+                duration: 270,
+                scale: {
+                    x: 1,
+                    y: 1
+                },
+            });
         });
-    })
-    .then(() => { 
-        return bells.animate({
-            rotate: 0,
-            duration: 270,
-            scale: {x: 1, y: 1},
-        });
-    });
 }
