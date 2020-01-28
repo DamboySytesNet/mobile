f2d59c7bd7693d03a280dbe6aba25ca438484cde
Adrian Zieliński <adrianziel24@wp.pl>
2020-01-28 23:56:33 +0100

    Register request

--- app/activities/registration/registration.js
+++ app/activities/registration/registration.js
@@ -1,13 +1,15 @@
 const observableModule = require("tns-core-modules/data/observable");
 const dialogsModule = require('tns-core-modules/ui/dialogs');
+const RegisterRequest = require('~/modules/request/registerRequest');
 let page;
 
 let pageData = new observableModule.fromObject({
-    name: '',
+    name: 'Adrian',
     surname: '',
     email: '',
     password: '',
-    repeatedPassword: ''
+    repeatedPassword: '',
+    loading: false
 })
 
 exports.back = args => {
@@ -22,9 +24,30 @@ exports.pageLoaded = args => {
 };
 
 exports.register = () => {
-    alert(pageData.name + '\n'
-        + pageData.surname + '\n'
-        + pageData.email + '\n'
-        + pageData.password + '\n'
-        + pageData.repeatedPassword);
+    if (validateEmail(pageData.get('email')) && 
+        pageData.get('name') !== '' &&
+        pageData.get('surname') !== '' &&
+        pageData.get('password') == pageData.get('repeatedPassword')) {
+            pageData.set('loading', true);
+            RegisterRequest.add(pageData.get('email'), pageData.get('name'),
+                                pageData.get('surname'), pageData.get('password'))
+                                .then(() => {
+                                    pageData.set('loading', false);
+                                    alert('Dziękujemy. Proszę potwierdzić rejestracje na mailu.');
+                                    page.frame.goBack();
+                                })
+                                .catch(() => {
+                                    alert('Nie udało się zarejestrować.');
+                                })
+        }
+        else {
+            alert('Błąd frontu');
+        }
+        
+}
+
+/** Validate email */
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
 }
